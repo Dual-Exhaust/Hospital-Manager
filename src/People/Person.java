af790148bb066243d5d137ffda0fcdd52c600f97
CodyBu <47016366+CodyBu@users.noreply.github.com>
2020-04-03 13:50:39 -0400

    Visits LinkedList added

--- src/People/Person.java
+++ src/People/Person.java
@@ -1,38 +1,58 @@
 package People;
 
+import java.util.LinkedList;
+
 public class Person implements PersonNode {
     //Forces child classes to have a name and descriptor
-    protected String name = "";
-    protected String descriptor = "";
+    protected String name;
+    protected String descriptor;
+    protected LinkedList<VisitNode> visits; 
 
-    //General constructor
+    /**Creates empty person object
+     * 
+     */
     public Person(){
-
+    	name = "";
+    	descriptor = "";
+    	visits = new LinkedList<>();
     }
 
-    //Probably the main constructor we will be using
+    /**Creates person object with parameters
+     * 
+     * @param name
+     * @param descriptor
+     */
     public Person(String name, String descriptor){
         this.name = name;
         this.descriptor = descriptor;
+        visits = new LinkedList<>();
     }
 
+    /**Returns name of person
+     * 
+     */
     public String getName(){
         return this.name;
     }
 
+    /**Returns descriptor of person
+     * 
+     */
     public String getDescriptor(){
         return this.descriptor;
     }
 
-    //Returns in format:
-        //  <Name> - <Description>
-    //  Unlike the sub-class specific toStrings, this will not blatantly say Doctor or Patient
+    /**Returns "Name - Description"
+     * 
+     */
     @Override
     public String toString() {
         return this.getName() + " - " + this.getDescriptor();
     }
 
-    // Try to set the person's name, if whatever it does breaks then it returns false and prints an error, else it will return true
+    /**Try to set the person's name, if whatever it does breaks then it returns false and prints an error, else it will return true
+     *
+     */
     public boolean setName(String name){
         try{
             this.name = name;
@@ -44,7 +64,9 @@ public class Person implements PersonNode {
         return true;
     }
 
-    // Try to set the person's description, if whatever it does breaks then it returns false and prints an error, else it will return true
+    /**Try to set the person's description, if whatever it does breaks then it returns false and prints an error, else it will return true
+     * 
+     */
     public boolean setDescriptor(String descriptor){
         try{
             this.descriptor = descriptor;
@@ -54,5 +76,21 @@ public class Person implements PersonNode {
             return false;
         }
         return true;
+    }
+    
+    /**Add visit to visits linked list
+     * 
+     * @param v
+     */
+    public void addVisit(VisitNode v) {
+    	visits.add(v);
+    }
+    
+    /**Remove visit from visits linked list
+     * 
+     * @param v
+     */
+    public void removeVisit(VisitNode v) {
+    	visits.remove(v);
     }
 }
